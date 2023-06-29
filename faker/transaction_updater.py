80ce78520ae294044d03cc3439166563e5f714a2
hoanguyen071710 <hoanguyen071710@gmail.com>
2023-06-29 18:23:40 +0700

    Refactor

--- faker/transaction_updater.py
+++ faker/transaction_updater.py
@@ -10,33 +10,41 @@ db_name = "stock_db"
 db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:3306/{db_name}"
 
 engine = create_engine(db_url)
-conn = engine.connect()
 
 # Randomize transaction count ratio to total users
-transaction_rate = random.uniform(0.5, 4)
-result_user = conn.execute(text(f"SELECT * FROM stock_db.User")).fetchall()
-transaction_count = round(len(result_user) * transaction_rate)
+def get_info(transaction_count):
+    result_user = conn.execute(text(f'SELECT * FROM stock_db.User')).fetchall()
+    transaction_count = transaction_count
 
-stock_info = conn.execute(text(f"SELECT * FROM stock_db.Stock")).fetchall()
+    stock_info = conn.execute(text(f"SELECT * FROM stock_db.Stock")).fetchall()
+    return result_user, stock_info, transaction_count
 
-transaction_list = []
-for i in range(transaction_count):
-    random_user_loc = random.randint(0, len(result_user) - 1)
-    random_stock_loc = random.randint(0, len(stock_info) - 1)
-    random_user = result_user[random_user_loc][0]
-    random_stock = stock_info[random_stock_loc][0]
-    random_quantity = random.randint(100, 1000000)
-    random_transaction_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    transaction_list.append(
-        str((random_user, random_stock, random_quantity, random_transaction_date))
-    )
+def user_stock_matching(transaction_count):
+    result_user, stock_info, transaction_count = get_info(transaction_count)
+    transaction_list = []
+    for i in range(transaction_count):
+        random_user_loc = random.randint(0, len(result_user) - 1)
+        random_stock_loc = random.randint(0, len(stock_info) - 1)
+        random_user = result_user[random_user_loc][0]
+        random_stock = stock_info[random_stock_loc][0]
+        random_quantity = random.randint(100, 1000000)
+        random_transaction_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        transaction_list.append(str((random_user, random_stock, random_quantity, random_transaction_date)))
+    return transaction_list
 
-transaction_query = ",".join(transaction_list)
-insert_query = f"""
-INSERT INTO stock_db.Transaction (user_id, stock_id, quantity, transaction_date)
-VALUES {transaction_query}
-"""
+def insert_into_db(transaction_count):
+    transaction_query = ','.join(user_stock_matching(transaction_count))
+    insert_query = f'''
+    INSERT INTO stock_db.Transaction (user_id, stock_id, quantity, transaction_date)
+    VALUES {transaction_query}
+    '''
+    conn.execute(text(insert_query))
 
-conn.execute(text(insert_query))
-conn.commit()
-conn.close()
+def updater(transaction_count):
+    insert_into_db(transaction_count)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    with engine.connect() as conn:
+        updater(20)
--- faker/user_updater.py
+++ faker/user_updater.py
@@ -11,43 +11,50 @@ db_name = "stock_db"
 db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:3306/{db_name}"
 
 engine = create_engine(db_url)
-conn = engine.connect()
-
-result = conn.execute(text("select * from stock_db.User")).fetchall()
 
 # Get the number of users that update email
-update_rate = random.uniform(0, 0.2)
-update_count = round(update_rate * len(result))
-# Get update user data
-update_user = conn.execute(
-    text(f"select * from stock_db.User order by rand() limit {update_count}")
-).fetchall()
-# Update these users info
+def update_rate():
+    result = conn.execute(text('SELECT * FROM stock_db.User')).fetchall()
+    update_rate = random.uniform(0,0.02)
+    update_count = round(update_rate * len(result))
+    # Get update user data
+    update_user = conn.execute(text(f'SELECT * FROM stock_db.User ORDER BY RAND() LIMIT {update_count}')).fetchall()
+    return update_user
 
-fake = Faker()
-for usr in update_user:
-    user_id = usr[0]
-    new_email = fake.email()
-    update_query = (
-        f"UPDATE stock_db.User SET email = '{new_email}' WHERE user_id = {user_id}"
-    )
-    conn.execute(text(update_query))
+
+# Update these users info
+def update_current_user():
+    fake = Faker()
+    update_user = update_rate()
+    for usr in update_user:
+        user_id = usr[0]
+        new_email = fake.email()
+        update_query = f"UPDATE stock_db.User SET email = '{new_email}' WHERE user_id = {user_id}"
+        conn.execute(text(update_query))
 
 # Create new users
-new_user_rate = random.uniform(0, 0.6)
-new_user_count = round(new_user_rate * len(result))
-new_user_list = []
-for new_usr in range(new_user_count):
-    user_name = fake.name()
-    user_email = fake.email()
-    new_user_list.append(str((user_name, user_email)))
-# Create query string to insert new users
-insert_val = ",".join(new_user_list)
-insert_query = f"""
-INSERT INTO stock_db.User (name, email) 
-VALUES {insert_val}
-"""
-conn.execute(text(insert_query))
+def create_new_user(new_user_count):
+    fake = Faker()
+    new_user_count = new_user_count
+    new_user_list = []
+    for new_usr in range(new_user_count):
+        user_name = fake.name()
+        user_email = fake.email()
+        new_user_list.append(str((user_name, user_email)))
+    # Create query string to insert new users
+    insert_val = ','.join(new_user_list)
+    insert_query = f'''
+    INSERT INTO stock_db.User (name, email) 
+    VALUES {insert_val}
+    '''
+    conn.execute(text(insert_query))
 
-conn.commit()
-conn.close()
+def updater(new_user_count):
+    update_current_user()
+    create_new_user(new_user_count)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    with engine.connect() as conn:
+        updater(10)
