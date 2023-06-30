61b1ec6477354d9cc0792c6bf3ed6f2b49d2c3e5
hoanguyen071710 <hoanguyen071710@gmail.com>
2023-06-30 21:30:50 +0700

    Lambda execution logic

--- faker/transaction_updater.py
+++ faker/transaction_updater.py
@@ -1,21 +1,16 @@
 from sqlalchemy import create_engine, text
 import random
 import datetime
+import os
 
 
-db_user = "admin"
-db_password = "password"
-db_host = "my-db-instance.cxjkxlbvg4uh.ap-northeast-1.rds.amazonaws.com"
-db_name = "stock_db"
-db_url = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:3306/{db_name}"
+db_url = os.environ['DB_URL']
 
 engine = create_engine(db_url)
 
 # Randomize transaction count ratio to total users
 def get_info(transaction_count):
     result_user = conn.execute(text(f'SELECT * FROM stock_db.User')).fetchall()
-    transaction_count = transaction_count
-
     stock_info = conn.execute(text(f"SELECT * FROM stock_db.Stock")).fetchall()
     return result_user, stock_info, transaction_count
 
@@ -27,7 +22,7 @@ def user_stock_matching(transaction_count):
         random_stock_loc = random.randint(0, len(stock_info) - 1)
         random_user = result_user[random_user_loc][0]
         random_stock = stock_info[random_stock_loc][0]
-        random_quantity = random.randint(100, 1000000)
+        random_quantity = random.randint(1, 10)
         random_transaction_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         transaction_list.append(str((random_user, random_stock, random_quantity, random_transaction_date)))
     return transaction_list
@@ -39,12 +34,8 @@ def insert_into_db(transaction_count):
     VALUES {transaction_query}
     '''
     conn.execute(text(insert_query))
-
-def updater(transaction_count):
-    insert_into_db(transaction_count)
     conn.commit()
-    conn.close()
 
-if __name__ == "__main__":
+def updater(event, context):
     with engine.connect() as conn:
-        updater(5)
+        insert_into_db(5)
