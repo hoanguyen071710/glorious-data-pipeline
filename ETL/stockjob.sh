6b37dafee18e7922235beb4820cf5dab037d007f
Dang Minh Hao <108931204+DangMinhHao@users.noreply.github.com>
2023-07-24 12:11:31 +0700

    Format structure (#34)
    * Combine ETL and etl folder then change files' name
    
    * Update files' name
    
    * Add file job.sh for stock and rename
    
    * Create snapshot_stockdb.py
    
    * Delete etl directory
    
    ---------
    
    Co-authored-by: HaoDang <dangminhhao9a21617@gmail.com>

---
+++ ETL/stockjob.sh
@@ -0,0 +1,2 @@
+python3 ./capture_latest_stock_records.py >> ./log/capture_latest_stock_records.txt 2>&1
+python3 ./snapshot_stockdb.py >> ./log/snapshot_stockdb.txt 2>&1
---
+++ ETL/userjob.sh
@@ -0,0 +1,2 @@
+python3 ./capture_latest_user_records.py >> ./log/capture_latest_user_records.txt 2>&1
+python3 ./snapshot_userdb.py >> ./log/snapshot_userdb.txt 2>&1
